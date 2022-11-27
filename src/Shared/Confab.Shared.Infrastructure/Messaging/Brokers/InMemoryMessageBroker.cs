d41f378c1a196ff6cf3d216c269d9a472466e2c8
piotr.witaszewski <piotr.witaszewski@gmail.com>
2022-11-27 22:50:42 +0100

    Asynchroniczna obsługa zdarzeń

--- src/Shared/Confab.Shared.Infrastructure/Messaging/Brokers/InMemoryMessageBroker.cs
+++ src/Shared/Confab.Shared.Infrastructure/Messaging/Brokers/InMemoryMessageBroker.cs
@@ -1,15 +1,21 @@
 ﻿using Confab.Shared.Abstractions.Messaging;
 using Confab.Shared.Abstractions.Modules;
+using Confab.Shared.Infrastructure.Messaging.Dispatchers;
 
 namespace Confab.Shared.Infrastructure.Messaging.Brokers;
 
 internal sealed class InMemoryMessageBroker : IMessageBroker
 {
     private readonly IModuleClient _moduleClient;
+    private readonly IAsyncMessageDispatcher _asyncMessageDispatcher;
+    private readonly MessagingOptions _messagingOptions;
 
-    public InMemoryMessageBroker(IModuleClient moduleClient)
+    public InMemoryMessageBroker(IModuleClient moduleClient, IAsyncMessageDispatcher asyncMessageDispatcher, 
+        MessagingOptions messagingOptions)
     {
         _moduleClient = moduleClient;
+        _asyncMessageDispatcher = asyncMessageDispatcher;
+        _messagingOptions = messagingOptions;
     }
 
     public async Task PublishAsync(params IMessage[] messages)
@@ -29,6 +35,12 @@ internal sealed class InMemoryMessageBroker : IMessageBroker
         var tasks = new List<Task>();
         foreach (var message in messages)
         {
+            if (_messagingOptions.UseBackgroundDispatcher)
+            {
+                await _asyncMessageDispatcher.PublishAsync(message);
+                continue;
+            }
+
             tasks.Add(_moduleClient.PublishAsync(message));
         }
 
---
+++ src/Shared/Confab.Shared.Infrastructure/Messaging/Dispatchers/BackgroundDispatcher.cs
@@ -0,0 +1,40 @@
+﻿using Confab.Shared.Abstractions.Modules;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Confab.Shared.Infrastructure.Messaging.Dispatchers;
+
+internal sealed class BackgroundDispatcher : BackgroundService
+{
+    private readonly IMessageChannel _messageChannel;
+    private readonly IModuleClient _moduleClient;
+    private readonly ILogger<BackgroundDispatcher> _logger;
+
+    public BackgroundDispatcher(IMessageChannel messageChannel, IModuleClient moduleClient, 
+        ILogger<BackgroundDispatcher> logger)
+    {
+        _messageChannel = messageChannel;
+        _moduleClient = moduleClient;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Running the backgroud dispatcher.");
+
+        await foreach(var message in _messageChannel.Reader.ReadAllAsync(stoppingToken))
+        {
+            try
+            {
+                await _moduleClient.PublishAsync(message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        _logger.LogInformation("Finished running the backgroud dispatcher.");
+    }
+}
